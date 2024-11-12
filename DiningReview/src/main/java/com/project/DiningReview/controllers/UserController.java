0eb9dee05ada021c1a59e421105a2db5ef041392
BrokenOnedroid <61624965+BrokenOnedroid@users.noreply.github.com>
2024-11-12 16:22:21 +0100

    working on User Controller

--- DiningReview/src/main/java/com/project/DiningReview/controllers/UserController.java
+++ DiningReview/src/main/java/com/project/DiningReview/controllers/UserController.java
@@ -1,14 +1,19 @@
 package com.project.DiningReview.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.DiningReview.entities.User;
 import com.project.DiningReview.repositories.UserRepository;
+
+import java.util.Optional;
+import java.util.List;
 
 @RequestMapping("/User")
 @RestController
@@ -20,16 +25,37 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    //create Entry in db
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void addUser(@RequestBody User user) {
         //check input 
-        checkForUser(user);
+        User userToSave = checkForUser(user);
         //save ifokay
-        userRepository.save(user);
+        userRepository.save(userToSave);
     }
 
-    public void checkForUser(User user) {
+    public User checkForUser(User user) {
+        Optional<User> userOptional;
+
+        // check if user var is valid (=> atleast contains displayname else query not possible)
+        // not sure but isEmpty needs the display name to exits == null should catch that case
+        if (user.getDisplayName() == null || user.getDisplayName().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Displayname is necessary");
+        }
+
+        userOptional = userRepository.findUserByDisplayName(user.getDisplayName());
+        // User already exits
+        if (userOptional.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "User already exits!");
+        }
         
+        return user;
     }
 }
